6877e82701bf08da9fc81a441dedf2df8fbcd804
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-17 17:19:43 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry Point
+ *  Description : use putchar to print output-alphabets in lowercase
+ *  Return: 0
+*/
+int main(void)
+{
+	int x = '0';
+
+	while (x < '10')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
