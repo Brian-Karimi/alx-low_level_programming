da0e8476ff2db754019eaf08505eedab644709a6
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-17 19:11:47 +0000

    Smile in the Mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ *  main - Entry Point
+ *  Description : use putchar to print output-alphabets in lowercase
+ *  Return: 0
+*/
+int main(void)
+{
+	int x = 'z';
+	
+	while (x = 'a')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
