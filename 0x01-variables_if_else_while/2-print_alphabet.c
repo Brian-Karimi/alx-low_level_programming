31e1dceda97730e789940b996d30d5f9d2f806aa
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-17 12:04:16 +0000

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  main - Entry Point
+ *  Description : Get a number and print its value input
+ *  And test/evaluate if it is positive, negative or zero
+ *  Return: 0
+*/
+int main(void)
+{
+	int n;
+	for (n= 'a'; n='z'; n++)
+		putchar(n);
+		putchar('\n');
+		getn();
+}
