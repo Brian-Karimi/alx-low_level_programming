c6810507d39176074605ec364cb67fe077a3f7d6
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-18 01:54:49 +0000

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+3#include <stdio.h>
+
+/**
+ *  main - Entry Point
+ *  Description: prints all possible combinations of numbers.
+ *  Numbers must be separated by ,, followed by a space, excluding 01 and 10
+ *  Numbers should be printed in their list value of combination
+ *  Return: 0
+*/
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = x + 1; y < 10; y++)
+		{
+			putchar (x + '0');
+			putchar (y + '0');
+
+			if (x < 9)
+			{
+				putchar(',');
+				putchar (' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
