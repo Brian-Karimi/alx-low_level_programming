e0f5ebb29f48794339df9f374dd86c75f8ff55de
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-18 00:52:37 +0000

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,10 +10,11 @@ int main(void)
 {
 	char x;
 
-	for (x = 0; x <= 16; x++)
-		putchar(x);
+	for (x = 0; x <= 9; x++)
+		putchar("%c\n", x);
 	for (x = 'a'; x <= 'f'; x++)
 		putchar(x);
+	
 	putchar('\n');
 
 	return (0);
