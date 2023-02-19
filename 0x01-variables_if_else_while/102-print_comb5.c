d6c22cd14c6e4ada199cce6bde69c2f377c80d62
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-19 14:45:55 +0000

    The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry Point
+ *  Description: all possible combinations of two two-digit numbers.
+ *  numbers should range from 0 to 99
+ *  Return: 0
+*/
+int main(void)
+{
+	int x;
+	int y;
+
+	for(x = "00", x <= "99", x++)
+	{
+		for(y = x + 1; y <= "99"; y++)
+		{
+			putchar(x);
+			putchar(y);
+			p
+		}
+	}
+}
