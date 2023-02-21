0f61c72047c9e28679046482597f00bd78333034
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-21 10:07:48 +0000

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <main.h>
 
 /**
- * _putchar- entry point
- * - writes the character c to stdout
+ * main - Entry Point
+ * _putchar- writes the character c to stdout
  * Prgram should print c
  *
  * Return:0
  */
-int _putchar(char c)
+int main(void)
 {
-	c = 65;
+	int x = 0;
+	char y[] = "_putchar";
+
+	for (x < 9; x++)
+	{
+		_putchar(y[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
