f13058f33fce9c199bc3797eae52453e20429f84
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-21 16:44:27 +0000

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,13 +10,13 @@
 */
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
-		else
-		{
-			return (0);
-		}
-	}_putchar('\n');
+	else 
+	{
+		return(0);
+	}
+	_putchar('\n');
 }
