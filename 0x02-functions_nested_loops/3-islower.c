06e0d014e8eb569a2eeb1b84443fe264ede62635
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-21 15:58:43 +0000

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,16 @@
 #include "main.h"
-/**
-*islower: checks if characters is lowercase
-* if character is lowercase, the function should return 0 else should return 1
-*
-* Return: 1
-*/
-int _islower(int c);
-{
-	c = 'a';
 
-	if (c >= 97; c < 123)
+/**
+ *_islower: checks for lowercase character
+_islower: checks for lowercase character.
+check if characters are in lowercase, 
+ * if lower, return 1
+ * else return 0
+ * Return 1
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c < 123)
 	{
 		return (1);
 	}
