fb6ded023c14ea75374ffe3e437e4426a2d062b0
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-21 20:48:40 +0000

    There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_last_digit- prints the last digit to input
+* @x: input number as an integer.
+*
+* Return: last digit.
+*/
+int print_last_digit(int x)
+{
+	int y;
+
+	y = x % 10;
+	if (y < 0)
+	{
+		_putchar(-y + '0');
+		return (-y);
+	}
+	else
+	{
+		_putchar(y + '0');
+		return (1);
+	}
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * jack_bauer- a funtion of time
+ * from 00:00 to 25:59
+ *
+ * return: void- no return
+ */
+void jack_bauer(void)
+{
+	int w, x, y, z;
+
+	for (w = '0'; w <= '2'; w++)
+	{
+		for (x = '0'; x <= 9; x++)
+		{
+			for (y = '0'; y <= 5; y++)
+			{
+				for (z = '0'; z <= '9'; z++)
+				{
+					if (w >= '2' && x >= '4')
+						break;
+					_putchar(w);
+					_putchar(x);
+					_putchar(':');
+					_putchar(y);
+					_putchar(z);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
