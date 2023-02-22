1d15bc07be7fec49365ca84b4c7ffb7a10df6eb8
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-22 22:18:19 +0000

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 
 /**
- * main- entry point 
- * Fibonnaci sequesnce, where new value is the sum of the two preceeding variables
+ * main- entry point
  *
- * return : 0
+ * Fibonnaci sequesnce
+ * where new value is the sum of the two preceeding variables
+ * Return: 0
  */
-int main(){
-    long int i = 1;
-    long int x = 2;
-    long int y;
-    long int new_Value;
-    
-    printf("%li", i , x);
-    
-    for ( y = 0; y < 47; y++)
-    {
-        new_Value = i + x;
-        
-        i = x;
-        x = new_Value;
-        
-        printf("%li", new_Value);
-    if (y <= 46)
-    {
-        putchar(',');
-        putchar(' ');
-    }
-    }
-    
-    printf("\n");
+int main(void)
+{
+	long int i = 1;
+	long int x = 2;
+	long int y;
+	long int new_Value;
+
+	printf("%li", i, x);
+	for (y = 0; y < 47; y++)
+	{
+		new_Value = i + x;
+		i = x;
+		x = new_Value;
+		printf("%li", new_Value);
+
+		if (y <= 4)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	printf("\n");
 }
