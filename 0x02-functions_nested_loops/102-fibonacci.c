8158488555dbf61486659a3aa938624cfe74cc72
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-22 21:55:58 +0000

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-//In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+
-//#advanced
-//Write a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.
-//The numbers should be separated by comma, followed by a space ,
-//You are allowed to use the standard library
-//You are not allowed to use any other library (You can’t use GMP etc…)
-//You are not allowed to use long long, malloc, pointers, arrays/tables, or structures
-//You are not allowed to hard code any Fibonacci number (except for 1 and 2)
+/**
+ * main- entry point 
+ * Fibonnaci sequesnce, where new value is the sum of the two preceeding variables
+ *
+ * return : 0
+ */
 int main(){
     long int i = 1;
     long int x = 2;
@@ -24,7 +22,7 @@ int main(){
         x = new_Value;
         
         printf("%li", new_Value);
-    if (y <= 94)
+    if (y <= 46)
     {
         putchar(',');
         putchar(' ');
