0ef059eb75b4ae8599d90cbb33b3e2a2ef93229f
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-22 13:09:07 +0000

    Like, Comment, Subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,16 +6,14 @@
 */
 int main(void)
 {
-	/**
-	 * int i;
-	 */
+	int i;
 
 	printf("Infinite loop coming :(\n");
 
-	/**
-	 * i = 0;
+	i = 0;
 
-	while (i < 10)
+	/**
+	 * while (i < 10)
 	{
 		putchar(i);
 	}
