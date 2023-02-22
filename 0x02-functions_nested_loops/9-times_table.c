3b767787b52b418653ff37e63b6f234a53938e29
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-22 01:30:03 +0000

    Learn Your Time Tables

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table-prints all tables starting from zero
+ *
+ * return: void no return
+ */
+void times_table(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar('0');
+		for (y = 1; y <= 9; y++)
+		{
+			z = x * y;
+			_putchar(',');
+			_putchar(' ');
+			if (z < 10)
+			{
+			_putchar(' ');
+			_putchar(z + '0');
+			}
+			else
+			{
+				_putchar((z % 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
