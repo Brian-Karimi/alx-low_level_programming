37301e2e6fd47327eeaf64bf18d919d1492dae06
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-23 11:25:12 +0000

    The Numbers Speak For Themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include  "main.h"
+
+/**
+ * print_numbers- prints all numbers in the range 0-9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	while (x >= 48 && x <= 57)
+	{
+		_putchar("%i", x);
+	}
+	_putchar('\n');
+
+	return (0);
+}
+
