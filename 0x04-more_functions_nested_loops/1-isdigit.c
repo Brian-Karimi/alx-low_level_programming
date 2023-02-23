c2ae875657215789706382c96a48de4329c50768
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-23 09:54:45 +0000

    Is Digit Correction

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
  * _isdigit- checks for a digit within 0 and 9
+ * @c: integer input
  * return ; 1 when c is a digit and
  * Return: Always 0 for otherwise
  */
 
-int main(void)
+int _isdigit(int c)
 {
-	char c;
-
 	while (c >= 48 && c <= 57)
 	{
 		return (1);
