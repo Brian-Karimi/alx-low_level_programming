280c779c85c13a08acde374c1ea538f01e04cc5d
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-23 08:55:42 +0000

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * _isdigit- checks for a digit within 0 and 9
+ * return ; 1 when c is a digit and
+ * Return: Always 0 for otherwise
+ */
+
+int main (void)
+{
+	char c;
+
+	while (c >= 48 && c <= 57; c ++)
+	{
+		printf("%c: %d\n", c, _isdigit(c)); 
+		return (1);
+	}
+	return (0);
+}
+
