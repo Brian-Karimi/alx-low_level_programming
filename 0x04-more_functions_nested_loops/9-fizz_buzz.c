6a99ffd377d4d1a7cd2a729b008866eaa68f63e1
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-24 02:29:28 +0000

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main-entyr point
+ * The “Fizz-Buzz test” is an interview question designed to help filter
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 100; x++)
+	{
+		if ((x % 15) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if ((x % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((x % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", x);
+		}
+		if (x < 100)
+		{
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
