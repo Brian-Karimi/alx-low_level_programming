b0ab266849cf577e7aa3bc409bc18b3480e3270e
Brian-Karimi <brianmwangik97@gmail.com>
2023-02-24 17:04:41 +0000

    Triangles Test 2

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,18 +11,18 @@ void print_triangle(int size)
 	int x, y;
 
 	if (size <= 0)
-		_putchar('\n');
+		putchar('\n');
 
 	for (x = 1; x <= size; x++)
 	{
 		for (y =1 ; y <= size - i; y++)
 		{
-			_putchar(' ');
+			putchar(' ');
 		}
 		for (y = 1; j <= i; y++)
 		{
-			_putchar('#');
+			putchar('#');
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
