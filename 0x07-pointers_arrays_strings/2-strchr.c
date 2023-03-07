c946036ca40780c8a10761719c004fbf239a2c94
Brian-Karimi <brianmwangik97@gmail.com>
2023-03-07 09:33:02 +0000

    strchr-locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,18 +3,22 @@
 /**
  * _strchr-locates a character in a string
  * @s: pointer to the declared string array
- * @c; character to locate from the array input 
- *
+ * @c: character to locate from the array input
  * Return: @c first occurence and null if character is not found
  */
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-		if (*s == c || *s == '\0')
+		if (*s == c)
 		{
 			return (s);
-			s++;
+		}
+		s++;
+		if (*s == c)
+		{
+			return (s);
 		}
 	}
+	return ('\0');
 }
