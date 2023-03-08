536e776fd70a8b9ec0f2d8e48683ef3e4ca5e176
Brian-Karimi <brianmwangik97@gmail.com>
2023-03-08 12:52:08 +0000

    Once an idea has taken hold of the brain it's almost impossible to eradicate

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _pow_recursion-returns the value of x raised to y
+ * @x: input value
+ * @y: the value t raise @x to
+ * Return: the new value of @x after raising to y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int evenNumber;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y % 2 == 0)
+	{
+		evenNumber = (_pow_recursion(x, y / 2));
+		return (evenNumber * evenNumber);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y));
+				}
+				}
+				}
