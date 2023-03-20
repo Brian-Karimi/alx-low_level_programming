2022d1a53caadc9f41ad9dd45b3f6f304cd1c540
Brian-Karimi <brianmwangik97@gmail.com>
2023-03-20 12:21:03 +0000

    File Name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * Description: prints the name of the file it was compiled from
+ * followed by a new line.
+ * Return: 0 success on success
+ **/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	{
+		return (0);
+	}
+}
