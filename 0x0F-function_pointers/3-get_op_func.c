a9966eb3a03532e61138839b7b679da518ce9c0f
Brian-Karimi <brianmwangik97@gmail.com>
2023-03-23 02:23:30 +0000

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * get_op_func-get ops function ptr of type char  array
+ * function accepts only two int type inputs
+ * @s: a char* pointing to a symbol form the program argument
+ * Return: one of the ops functions for computation
+ **/
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (*s == *ops[i].ops)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
