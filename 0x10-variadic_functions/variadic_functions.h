2d3e24778799ca1df7581e442c7259fb710326b7
Brian-Karimi <brianmwangik97@gmail.com>
2023-03-23 20:33:18 +0000

    Header File Update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,19 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
 
-int _putchar(char c);
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct print
+{
+	char format;
+	void (*function)(va_list);
+} print_t;
+
+int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif
