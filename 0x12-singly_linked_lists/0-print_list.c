ea168b8d68f69aaa7e996197b8ea85482d301b36
Brian-Karimi <brianmwangik97@gmail.com>
2023-03-31 01:27:48 +0000

    Print List

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,10 +9,14 @@ size_t print_list(const list_t *h)
 {
 	size_t count;
 
-	for (count = 0; h; count++)
+	while (h)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		(h->str != NULL) ? printf("[%d] %s\n", h->len, h->str) :
+			printf("[0] (nil)\n");
+
+		count++;
 		h = h->next;
 	}
+
 	return (count);
 }
