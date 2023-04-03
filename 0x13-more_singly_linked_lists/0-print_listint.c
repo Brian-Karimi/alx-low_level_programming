409a85fa7770bdd37d5ab3a3cbfdc5e1d612f99d
Brian-Karimi <brianmwangik97@gmail.com>
2023-04-03 10:05:25 +0000

    Print List Cont

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_list-prints all elemets of a listint_t list
+ * @h: pointer to the singly linked list
+ * Return: number of nodes
+ **/
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
