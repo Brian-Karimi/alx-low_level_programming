0ee3becf67bdfaa23d4d0b28ea7348b6256713b0
Brian-Karimi <brianmwangik97@gmail.com>
2023-04-03 14:05:26 +0000

    Free Struct

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint-frees a listint_t list
+ * @head: pointer to the first node
+ * Return: nothing
+ **/
+void free_listint2(listint_t **head)
+{
+	listint_t *temp_node, *current_node;
+
+	if (head != NULL)
+	{
+		current_node = *head;
+
+	while ((temp_node = current_node) != NULL)
+	{
+		current_node = current_node->next;
+		free(temp_node);
+	}
+
+	*head = NULL;
+	
+	}
+}
