c3f236fde3287111868466203355a7e6fea1374f
Brian-Karimi <brianmwangik97@gmail.com>
2023-04-05 01:56:02 +0000

    Reverse Nodes in the List

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * reverse_listint-reverses a listint_t linked list
+ * @head: Pointer to the head
+ * Return: reversed linked list
+ **/
+
+listint_t *reverse_listint(listint_t **head)
+{
+	head++;
+	return (0);
+
+}
