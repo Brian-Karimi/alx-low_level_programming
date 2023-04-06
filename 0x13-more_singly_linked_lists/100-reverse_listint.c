da20b878acd99dd6326dcfc5f9f5c7f6e71667b9
Brian-Karimi <brianmwangik97@gmail.com>
2023-04-06 02:30:48 +0000

    Reverse List Revision Update

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,7 +8,30 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-	head++;
-	return (0);
+	listint_t *ptr, *temp_node;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	if (*head != NULL)
+	{
+		ptr = *head;
+		temp_node = (*head)->next;
+		*head = (*head)->next;
+
+		ptr->next = NULL;
+
+		while (*head)
+		{
+			*head = (*head)->next;
+			temp_node->next = ptr;
+
+			ptr = temp_node;
+			temp_node = *head;
+		}
+		*head = ptr;
+	}
+	return (*head);
 }
