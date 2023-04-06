0ebf281aab08028d3290ded09a4be6119e9875d1
Brian-Karimi <brianmwangik97@gmail.com>
2023-04-06 02:21:21 +0000

    Print List-Safe Version

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe-prints listint_t linked list
+ * @head: pointer to the first node in the linked list
+ * Return: nodes
+ **/
+
+size_t print_listint_safe(const listint_t *head)
+{
+	head++;
+	return (size_t);
+}
