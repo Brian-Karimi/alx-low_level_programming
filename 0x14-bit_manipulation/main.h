57be7d2c2f0edeeaad0c2e3822d9641ee9bc3b89
Brian-Karimi <brianmwangik97@gmail.com>
2023-04-08 17:03:35 +0000

    Header File Update

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stddef.h>
+#include <limits.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -10,5 +11,5 @@ void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
-
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 #endif
