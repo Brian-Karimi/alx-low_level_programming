c2dbcff3a7e055e1817581bbe4d53b23b73efb08
Brian-Karimi <brianmwangik97@gmail.com>
2023-04-08 08:12:43 +0000

    10-Return Value of Bit at  Given Index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * get_bit-returns the value of a bot at a given index
+ * @index: index for the targeted bit
+ * @n: input for long int
+ * Return: the value  of bit at index
+ **/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return (index > 30) ? -1 : ((n >> index) & 1);
+}
