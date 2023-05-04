599fc9d01049e20cc0302edf215be7efdf575b51
Brian-Karimi <brianmwangik97@gmail.com>
2023-05-04 20:43:45 +0300

    Binary Represantation of a Number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a Number
+ * @n: integer  to represent the binary value
+ * Return: void
+*/
+void print_binary(unsigned long int n)
+{
+    int index;
+    
+    for (index = sizeof(n) * 8 - 1; index >= 0; index--)
+    {
+        if ((n & (1UL << index)) == 0)
+        {
+            printf("0");
+        }
+        else
+        {
+            printf("1");
+        }
+    }
+}
