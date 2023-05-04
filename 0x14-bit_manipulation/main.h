0539c23d24473aa331c0473e4c40754e3ee39883
Brian-Karimi <brianmwangik97@gmail.com>
2023-05-04 17:22:02 +0300

    Header File

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stddef.h>
+#include <limits.h>
+#include <string.h>
+
+
+unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
+
+#endif  /** My Header File-Bit Manipulation Tasks*/
